17af95a70e1212fb59e57b4cfce22fbe261f6e3d
gosurim <su020900@gmail.com>
2025-08-27 01:59:31 +0900

    feat: 감정테스트결과

---
+++ src/main/java/com/example/demo/domain/emotionTest/EmotionTestResult.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain.emotionTest;
+
+import com.example.demo.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "EmotionTestResult")
+public class EmotionTestResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private EmotionTestQuestion emotionTestQuestion;
+
+    @Column(nullable = false)
+    private Integer resultNum;
+
+    protected EmotionTestResult() {
+    }
+}
