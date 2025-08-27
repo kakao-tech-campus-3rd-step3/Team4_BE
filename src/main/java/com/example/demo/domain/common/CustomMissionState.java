49214158d040277cc54f32fe1ebed535bea5207d
BSY3510 <baeksy3510@gmail.com>
2025-08-27 16:09:47 +0900

    feat(domain): 공통 Enum 클래스 추가

---
+++ src/main/java/com/example/demo/domain/common/CustomMissionState.java
@@ -0,0 +1,7 @@
+package com.example.demo.domain.common;
+
+public enum CustomMissionState {
+    WAITING,    // 필터 대기 상태
+    FILTERED,   // 승인 대기 상태(필터링은 되었지만 승인이 안된 경우)
+    REJECTED    // 승인 거절 상태
+}
