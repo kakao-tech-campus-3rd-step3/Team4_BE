20ed0e5583f4cf903d76cb8815b7c908f998921d
thstkddnr20 <thstkddnr20@gmail.com>
2025-08-29 17:57:46 +0900

    refactor: 사용하지 않는 import문 제거

--- src/main/java/com/example/demo/domain/emotionTest/EmotionTestQuestion.java
+++ src/main/java/com/example/demo/domain/emotionTest/EmotionTestQuestion.java
@@ -1,17 +1,6 @@
 package com.example.demo.domain.emotionTest;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "emotion_test_question")
