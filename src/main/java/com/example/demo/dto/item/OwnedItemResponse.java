3cfa394def6d363f41b5c7902361bf46616eeb6d
thstkddnr20 <thstkddnr20@gmail.com>
2025-09-07 19:44:19 +0900

    fix: 응답 dto 불필요한 setter 제거

--- src/main/java/com/example/demo/dto/item/OwnedItemResponse.java
+++ src/main/java/com/example/demo/dto/item/OwnedItemResponse.java
@@ -4,18 +4,16 @@ import com.example.demo.domain.cat.Item;
 import com.example.demo.domain.cat.ItemCategoryEnum;
 import com.example.demo.domain.cat.OwnedItem;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class OwnedItemResponse {
-    private Long id;
-    private ItemCategoryEnum category;
-    private String name;
-    private String imageUrl;
-    private Float offsetX;
-    private Float offsetY;
-    private Boolean isUsed;
+    private final Long id;
+    private final ItemCategoryEnum category;
+    private final String name;
+    private final String imageUrl;
+    private final Float offsetX;
+    private final Float offsetY;
+    private final Boolean isUsed;
 
     public OwnedItemResponse(OwnedItem ownedItem) {
         Item item = ownedItem.getItem();
