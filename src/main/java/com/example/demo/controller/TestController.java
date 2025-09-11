13586e50e911c8a2ebeb4069da8f7dce9af2e01b
javachip <79399385+thstkddnr20@users.noreply.github.com>
2025-09-11 19:32:23 +0900

    Revert "Revert "CORS 설정""

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<Void> test() {
+        return ResponseEntity.ok().build();
+    }
+
+}
