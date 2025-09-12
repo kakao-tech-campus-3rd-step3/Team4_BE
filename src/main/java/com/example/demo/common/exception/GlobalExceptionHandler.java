787a2be6cbea32bc2c91606dd0182e99b8bb7fcd
thstkddnr20 <thstkddnr20@gmail.com>
2025-09-12 18:16:18 +0900

    feat: handler 찾지 못할 경우 예외 처리

--- src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.example.demo.common.exception;
 
 import com.example.demo.common.exception.errorcode.ErrorCode;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 @Slf4j
@@ -19,32 +22,32 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(
-            BusinessException e) {
+        BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
 
         String paramsStr = requestContext.getQueryParams().entrySet().stream()
-                .map(m -> m.getKey() + ":" + Arrays.toString(m.getValue()))
-                .collect(Collectors.joining(", "));
+            .map(m -> m.getKey() + ":" + Arrays.toString(m.getValue()))
+            .collect(Collectors.joining(", "));
 
         if (errorCode.getStatus().is5xxServerError()) {
             log.error(
-                    "Internal server error occurred (errorCode={}): message={}, userId={}, requestUri={}, httpMethod={}, params={}",
-                    e.getErrorCode().getCode(),
-                    e.getClientMessage(),
-                    requestContext.getUserId(),
-                    requestContext.getRequestUri(),
-                    requestContext.getHttpMethod(),
-                    paramsStr,
-                    e);
+                "Internal server error occurred (errorCode={}): message={}, userId={}, requestUri={}, httpMethod={}, params={}",
+                e.getErrorCode().getCode(),
+                e.getClientMessage(),
+                requestContext.getUserId(),
+                requestContext.getRequestUri(),
+                requestContext.getHttpMethod(),
+                paramsStr,
+                e);
         } else {
             log.warn(
-                    "Business exception occurred (errorCode={}): message={}, userId={}, requestUri={}, httpMethod={}, params={}",
-                    e.getErrorCode().getCode(),
-                    e.getClientMessage(),
-                    requestContext.getUserId(),
-                    requestContext.getRequestUri(),
-                    requestContext.getHttpMethod(),
-                    paramsStr
+                "Business exception occurred (errorCode={}): message={}, userId={}, requestUri={}, httpMethod={}, params={}",
+                e.getErrorCode().getCode(),
+                e.getClientMessage(),
+                requestContext.getUserId(),
+                requestContext.getRequestUri(),
+                requestContext.getHttpMethod(),
+                paramsStr
             );
         }
 
@@ -56,6 +59,14 @@ public class GlobalExceptionHandler {
         log.error("Unhandled exception occurred: {}", e.getMessage(), e);
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ErrorResponse.internal());
+            .body(ErrorResponse.internal());
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "정의되지 않은 API 요청입니다.");
+        body.put("path", e.getRequestURL());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 }
