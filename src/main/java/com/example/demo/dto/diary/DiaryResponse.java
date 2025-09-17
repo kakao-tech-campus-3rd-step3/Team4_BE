6de50fa614ce91af59115147435cb67fb17ee10d
keroro7729 <keroro06108@gmail.com>
2025-09-18 04:06:34 +0900

    fix: DiaryResponse 피드백 첨부 형식 수정
    Signed-off-by: keroro7729 <keroro06108@gmail.com>

--- src/main/java/com/example/demo/dto/diary/DiaryResponse.java
+++ src/main/java/com/example/demo/dto/diary/DiaryResponse.java
@@ -1,7 +1,6 @@
 package com.example.demo.dto.diary;
 
 import com.example.demo.domain.diary.Diary;
-import com.example.demo.domain.diary.DiaryFeedback;
 import com.example.demo.domain.diary.EmotionEnum;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -11,13 +10,13 @@ public class DiaryResponse {
 
     private EmotionEnum emotion;
     private String content;
-    private DiaryFeedback feedback;
+    private String feedback;
     private LocalDateTime createdAt;
 
     public DiaryResponse(Diary diary) {
         this.emotion = diary.getEmotion();
         this.content = diary.getContent();
-        this.feedback = diary.getFeedback();
+        this.feedback = diary.getFeedback().getContent();
         this.createdAt = diary.getCreatedAt();
     }
 }
