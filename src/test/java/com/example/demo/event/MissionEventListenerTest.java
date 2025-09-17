f9ca671515f612b4efa0d5de500f908e7277208e
thstkddnr20 <thstkddnr20@gmail.com>
2025-09-17 11:06:43 +0900

    test: 미션 이벤트 -> 서비스 호출 테스트

--- src/test/java/com/example/demo/event/MissionEventListenerTest.java
+++ src/test/java/com/example/demo/event/MissionEventListenerTest.java
@@ -1,104 +1,66 @@
 package com.example.demo.event;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
-import com.example.demo.domain.mission.Mission;
-import com.example.demo.domain.mission.MissionCategoryEnum;
-import com.example.demo.domain.user.User;
-import com.example.demo.domain.userEmotion.UserEmotion;
-import com.example.demo.dto.plan.PlanResponse;
-import com.example.demo.repository.MissionRepository;
-import com.example.demo.repository.UserEmotionRepository;
-import com.example.demo.service.MissionService;
-import com.example.demo.service.PlanCommandService;
-import com.example.demo.service.PlanQueryService;
-import jakarta.persistence.EntityManager;
-import java.time.LocalDate;
+import com.example.demo.event.mission.MissionCompletionEvent;
+import com.example.demo.event.mission.MissionEventListener;
+import com.example.demo.event.mission.MissionExposureEvent;
+import com.example.demo.event.mission.MissionSelectionEvent;
+import com.example.demo.service.mission.MissionCounterService;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class MissionEventListenerTest {
 
-    @Autowired
-    MissionService missionService;
+    @Mock
+    private MissionCounterService counterService;
 
-    @Autowired
-    UserEmotionRepository userEmotionRepository;
+    @InjectMocks
+    private MissionEventListener listener;
 
-    @Autowired
-    MissionRepository missionRepository;
+    @Test
+    void 추천_이벤트가_발생하면_CounterService의_addExposureDelta가_호출된다() {
+        //when
+        Long missionId = 1L;
+        MissionExposureEvent event = new MissionExposureEvent(List.of(missionId));
 
-    @Autowired
-    PlanCommandService planCommandService;
+        //then
+        listener.handleMissionExposureEvent(event);
 
-    @Autowired
-    PlanQueryService planQueryService;
-
-    @Autowired
-    EntityManager em;
-
-    User user;
-    Mission mission1;
-    Mission mission2;
-
-    @BeforeEach
-    void setUp() {
-        user = User.of("야호", "yaho@test.com");
-        UserEmotion userEmotion = new UserEmotion(user, 1, 1, 1, 1, 1, 1);
-        userEmotionRepository.save(userEmotion);
-
-        //mission1, mission2 노출, 선택, 완료 횟수 모두 0
-        mission1 = missionRepository.save(
-            new Mission("책 읽기", MissionCategoryEnum.REFRESH, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1));
-
-        mission2 = missionRepository.save(
-            new Mission("책 10권 읽기", MissionCategoryEnum.DAILY, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1));
+        //then
+        verify(counterService).addExposureDelta(List.of(missionId));
     }
 
     @Test
-    void 미션_추천_시_해당_미션의_노출_횟수가_증가한다() {
-        //when
-        missionService.getRecommendedMissions(user);
+    void 선택_이벤트가_발생하면_선택_횟수가_증가한다() {
+        // given
+        Long missionId = 1L;
+        MissionSelectionEvent event = new MissionSelectionEvent(missionId);
 
-        //then
-        mission1 = missionRepository.findById(mission1.getId()).get();
-        mission2 = missionRepository.findById(mission2.getId()).get();
+        // when
+        listener.handleMissionSelectionEvent(event);
 
-        assertThat(mission1.getExposureCount()).isEqualTo(1);
-        assertThat(mission2.getExposureCount()).isEqualTo(1);
+        // then
+        verify(counterService).addSelectionDelta(missionId);
 
     }
 
     @Test
-    void 미션_선택_시_해당_미션의_선택_횟수가_증가한다() {
-        //when
-        planCommandService.addMissionToPlan(mission1.getId(), user);
+    void 완료_이벤트가_발생하면_완료_횟수가_증가한다() {
+        // given
+        Long missionId = 1L;
+        MissionCompletionEvent event = new MissionCompletionEvent(missionId);
 
-        //then
-        mission1 = missionRepository.findById(mission1.getId()).get();
+        // when
+        listener.handleMissionCompletionEvent(event);
 
-        assertThat(mission1.getSelectionCount()).isEqualTo(1);
-
-    }
-
-    @Test
-    void 미션_완료_시_해당_미션의_완료_횟수가_증가한다() {
-        //given
-        planCommandService.addMissionToPlan(mission1.getId(), user);
-        List<PlanResponse> plans = planQueryService.getPlans(LocalDate.now(), false, user);
-        PlanResponse planResponse = plans.get(0);
-
-        //when
-        planCommandService.updatePlanStatus(planResponse.getId(), true, user);
-
-        mission1 = missionRepository.findById(mission1.getId()).get();
-
-        //then
-        assertThat(mission1.getCompletionCount()).isEqualTo(1);
+        // then
+        verify(counterService).addCompletionDelta(missionId);
     }
 
 }
