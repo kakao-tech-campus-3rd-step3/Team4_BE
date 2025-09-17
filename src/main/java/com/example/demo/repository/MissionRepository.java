2e97c41deb45805fe7a9c030d281f8127d3d78a1
thstkddnr20 <thstkddnr20@gmail.com>
2025-09-17 14:29:02 +0900

    refactor: 불필요 메서드 제거

--- src/main/java/com/example/demo/repository/MissionRepository.java
+++ src/main/java/com/example/demo/repository/MissionRepository.java
@@ -5,7 +5,6 @@ import com.example.demo.domain.mission.MissionCategoryEnum;
 import com.example.demo.dto.mission.MissionAverageResponse;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
