c5180b585af555712f6bcce4d22ab96376339983
thstkddnr20 <thstkddnr20@gmail.com>
2025-09-24 12:45:40 +0900

    refactor: cat 저장 시 item 같이 저장

--- src/main/java/com/example/demo/cat/infrastructure/CatRepositoryImpl.java
+++ src/main/java/com/example/demo/cat/infrastructure/CatRepositoryImpl.java
@@ -1,9 +1,13 @@
 package com.example.demo.cat.infrastructure;
 
 import com.example.demo.cat.domain.Cat;
+import com.example.demo.cat.domain.Item;
 import com.example.demo.cat.infrastructure.jpa.CatEntity;
 import com.example.demo.cat.infrastructure.jpa.CatJpaRepository;
+import com.example.demo.cat.infrastructure.jpa.ItemEntity;
+import com.example.demo.cat.infrastructure.jpa.ItemJpaRepository;
 import com.example.demo.cat.service.CatRepository;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -13,9 +17,12 @@ import org.springframework.stereotype.Repository;
 public class CatRepositoryImpl implements CatRepository {
 
     private final CatJpaRepository catJpaRepository;
+    private final ItemJpaRepository itemJpaRepository;
 
     @Override
     public Cat save(Cat cat) {
+        List<Item> items = cat.getItems();
+        itemJpaRepository.saveAll(items.stream().map(ItemEntity::fromModel).toList());
         return catJpaRepository.save(CatEntity.fromModel(cat)).toModel();
     }
 
