dba2533301fd07a2592e3f7793a583d7f890d1dc
BSY3510 <baeksy3510@gmail.com>
2025-09-26 03:05:17 +0900

    refactor: response로 m customMission에서 mission 으로 반영

--- src/main/java/com/example/demo/mission/controller/dto/MissionResponse.java
+++ src/main/java/com/example/demo/mission/controller/dto/MissionResponse.java
@@ -1,8 +1,7 @@
 package com.example.demo.mission.controller.dto;
 
+import com.example.demo.mission.Mission;
 import com.example.demo.mission.MissionCategoryEnum;
-import com.example.demo.mission.custom.domain.CustomMission;
-import com.example.demo.mission.custom.domain.CustomMissionStateEnum;
 import lombok.Getter;
 
 @Getter
@@ -11,13 +10,11 @@ public class MissionResponse {
     private final Long id;
     private final String content;
     private final MissionCategoryEnum category;
-    private final CustomMissionStateEnum state;
 
-    public MissionResponse(CustomMission customMission) {
-        this.id = customMission.getId();
-        this.content = customMission.getContent();
-        this.category = customMission.getCategory();
-        this.state = customMission.getState();
+    public MissionResponse(Mission mission) {
+        this.id = mission.getId();
+        this.content = mission.getContent();
+        this.category = mission.getCategory();
     }
 
 }
