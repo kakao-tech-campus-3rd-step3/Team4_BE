d2df42cf0f02bd378fce15f6716fbd2c10e3d60f
thstkddnr20 <thstkddnr20@gmail.com>
2025-09-26 11:16:04 +0900

    refactor: 유저가 활동을 한다는 의미에서 ActivityService로 이름 변경

--- src/main/java/com/example/demo/mission/regular/service/ActivityService.java
+++ src/main/java/com/example/demo/mission/regular/service/ActivityService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MissionTrackingService {
+public class ActivityService {
 
     private final PlanInternalService planInternalService;
     private final MissionCounterService missionCounterService;
