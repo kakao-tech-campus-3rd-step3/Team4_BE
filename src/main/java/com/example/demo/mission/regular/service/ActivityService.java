18cea5bc9a65c988d0fb3cf2f464080b46db445e
gosurim <su020900@gmail.com>
2025-10-01 23:17:57 +0900

    feat: 반환 타입 변경

--- src/main/java/com/example/demo/mission/regular/service/ActivityService.java
+++ src/main/java/com/example/demo/mission/regular/service/ActivityService.java
@@ -1,7 +1,11 @@
 package com.example.demo.mission.regular.service;
 
+import com.example.demo.mission.regular.domain.MissionScore;
 import com.example.demo.mission.regular.service.counter.MissionCounterService;
+import com.example.demo.mission.regular.service.score.MissionScoreMinMax;
 import com.example.demo.plan.controller.dto.PlanCreateRequest;
+import com.example.demo.plan.domain.MissionType;
+import com.example.demo.plan.domain.Plan;
 import com.example.demo.plan.service.PlanInternalService;
 import com.example.demo.user.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +17,7 @@ public class ActivityService {
 
     private final PlanInternalService planInternalService;
     private final MissionCounterService missionCounterService;
+    private final MissionEmotionService missionEmotionService;
 
     public void addMissionToPlan(PlanCreateRequest request, User user) {
         Long missionId = planInternalService.addMissionToPlan(request, user);
@@ -20,9 +25,12 @@ public class ActivityService {
     }
 
     public void updatePlanStatus(Long planId, boolean isDone, User user) {
-        Long missionId = planInternalService.updatePlanStatus(planId, isDone, user);
+        Plan plan = planInternalService.updatePlanStatus(planId, isDone, user);
         if (isDone) {
-            missionCounterService.addCompletionDelta(missionId);
+            missionCounterService.addCompletionDelta(plan.getMissionId());
+            if (plan.getMissionType() == MissionType.REGULAR) {
+                missionEmotionService.mission(user, plan.getMissionId());
+            }
         }
     }
 
