d3d8b84a6ffc0b9617b63fca5810a2c908199aeb
BSY3510 <baeksy3510@gmail.com>
2025-10-04 18:41:43 +0900

    refactor: 불필요 메서드 제거

--- src/main/java/com/example/demo/chat/infrastructure/jpa/MessageJpaRepository.java
+++ src/main/java/com/example/demo/chat/infrastructure/jpa/MessageJpaRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.chat.infrastructure.jpa;
 
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +8,4 @@ public interface MessageJpaRepository extends JpaRepository<Message, Long> {
 
     Page<Message> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
 
-    List<Message> findTop10ByUserIdOrderByCreatedAtDesc(Long userId);
 }
