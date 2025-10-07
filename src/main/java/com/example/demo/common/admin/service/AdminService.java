782886c10d66cbc74671cd592db54a4707b92567
surim <su020900@gmail.com>
2025-10-07 19:37:00 +0900

    feat: 미션 min max 캐시 삭제 어노테이션

--- src/main/java/com/example/demo/common/admin/service/AdminService.java
+++ src/main/java/com/example/demo/common/admin/service/AdminService.java
@@ -5,6 +5,7 @@ import com.example.demo.product.domain.DisplayImage;
 import com.example.demo.product.domain.ProductItem;
 import com.example.demo.product.service.ProductItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,12 +18,17 @@ public class AdminService {
 
     public void registerItem(CreateProductItemRequest request) {
         ProductItem product = new ProductItem(
-            request.getName(),
-            request.getPrice(),
-            request.getCategory(),
-            new DisplayImage(request.getImageUrl(), request.getOffsetX(), request.getOffsetY())
+                request.getName(),
+                request.getPrice(),
+                request.getCategory(),
+                new DisplayImage(request.getImageUrl(), request.getOffsetX(), request.getOffsetY())
         );
 
         productItemRepository.save(product);
+    }
+
+    @CacheEvict(value = "missionMinMax", key = "'ALL'")
+    // ↑ 미션 수정 시 미션 6지표 min, max 캐시 삭제(ex 커스텀 미션 -> 정규 미션: 캐시 삭제 후 다음 조회 시 재계산됨)
+    public void updateMission() {
     }
 }
