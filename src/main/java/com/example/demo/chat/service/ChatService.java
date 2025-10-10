5fa6794a50f9c25b6c3bd1ecca0b79b98c27baa9
keroro7729 <keroro06108@gmail.com>
2025-10-10 16:45:21 +0900

    feat: 프롬프트 수정 및 ChatService 위험도, 텀메모리 활용 추가

--- src/main/java/com/example/demo/chat/service/ChatService.java
+++ src/main/java/com/example/demo/chat/service/ChatService.java
@@ -2,13 +2,19 @@ package com.example.demo.chat.service;
 
 import com.example.demo.chat.controller.dto.ChatResponse;
 import com.example.demo.chat.controller.dto.MessageResponse;
+import com.example.demo.chat.infrastructure.jpa.LongTermMemory;
+import com.example.demo.chat.infrastructure.jpa.LongTermMemoryJpaRepository;
 import com.example.demo.chat.infrastructure.jpa.Message;
 import com.example.demo.chat.infrastructure.jpa.Sender;
 import com.example.demo.common.infrastructure.openai.OpenAiClient;
 import com.example.demo.common.infrastructure.openai.dto.OpenAiResponse;
+import com.example.demo.emotion.domain.DangerState;
+import com.example.demo.emotion.domain.Emotion;
+import com.example.demo.emotion.service.EmotionRepository;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -24,22 +30,33 @@ public class ChatService {
 
     private final OpenAiClient openAiClient;
     private final MessageRepository messageRepository;
+    private final EmotionRepository emotionRepository;
+    private final LongTermMemoryJpaRepository longTermMemoryJpaRepository;
 
     public ChatResponse postMessage(String messageContent, Long userId) {
-        List<Message> recentMessages = messageRepository.findTopNByUserIdOrderByCreatedAtDesc(
-            userId, 10);
-        List<String> context = recentMessages.stream()
-            .map(Message::getContent)
-            .collect(Collectors.toList());
-        Collections.reverse(context);
+        List<String> context = fetchContext(userId);
+        LongTermMemory longTermMemory = fetchLongTermMemory(userId);
+        OpenAiResponse openAiResponse = openAiClient.getChatResponse(messageContent, context, longTermMemory.getMemory());
 
-        OpenAiResponse openAiResponse = openAiClient.getChatResponse(messageContent, context);
+        Optional<Integer> dangerScore = openAiResponse.getDangerScore();
+        Optional<Emotion> emotion = emotionRepository.findById(userId);
+        if (dangerScore.isPresent() && emotion.isPresent()) {
+            emotion.get().applyDangerLevel(dangerScore.get());
+            DangerState state = emotion.get().getDangerState();
+            switch (state) {
+                case HIGH_DANGER -> longTermMemory.appendHighDangerMessage();
+                case BURST -> longTermMemory.appendBurstDangerMessage();
+                case CHRONIC -> longTermMemory.appendChronicDangerMessage();
+                case STABLE -> longTermMemory.clearDangerMessage();
+            }
+            emotionRepository.save(emotion.get());
+            longTermMemoryJpaRepository.save(longTermMemory);
+        }
 
-        Message userMessage = new Message(userId, Sender.USER, messageContent,
+        Message userMessage = new Message(userId, Sender.USER, messageContent, dangerScore.orElseGet(() -> null),
             LocalDateTime.now());
         messageRepository.save(userMessage);
-
-        Message catMessage = new Message(userId, Sender.CAT, openAiResponse.getMessage(),
+        Message catMessage = new Message(userId, Sender.CAT, openAiResponse.getMessage(), dangerScore.orElseGet(() -> null),
             LocalDateTime.now());
         messageRepository.save(catMessage);
 
@@ -52,5 +69,18 @@ public class ChatService {
         return messageRepository.findByUserId(userId, pageable)
             .map(message -> new MessageResponse(message.getId(), message.getContent(), null,
                 message.getCreatedAt()));
+    }
+
+    private List<String> fetchContext(Long userId) {
+        List<String> context = messageRepository.findTopNByUserIdOrderByCreatedAtDesc(userId, 10).stream()
+            .map(Message::getContent)
+            .collect(Collectors.toList());
+        Collections.reverse(context);
+        return context;
+    }
+
+    private LongTermMemory fetchLongTermMemory(Long userId) {
+        return longTermMemoryJpaRepository.findById(userId)
+            .orElseGet(() -> longTermMemoryJpaRepository.save(new LongTermMemory(userId)));
     }
 }
