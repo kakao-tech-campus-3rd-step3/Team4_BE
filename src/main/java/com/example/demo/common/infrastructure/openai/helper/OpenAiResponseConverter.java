37b1db66709072e5c821d79583c23bedbe56c000
thstkddnr20 <thstkddnr20@gmail.com>
2025-10-13 22:24:50 +0900

    refactor: 불필요 주석 제거

--- src/main/java/com/example/demo/common/infrastructure/openai/helper/OpenAiResponseConverter.java
+++ src/main/java/com/example/demo/common/infrastructure/openai/helper/OpenAiResponseConverter.java
@@ -8,9 +8,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class OpenAiResponseConverter {
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
