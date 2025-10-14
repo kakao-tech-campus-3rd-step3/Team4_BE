b8310c9eb4abfcc7ffe2ea027eef2c41c5172bd9
keroro7729 <keroro06108@gmail.com>
2025-10-15 06:14:17 +0900

    리뷰 반영

--- src/main/java/com/example/demo/emotion/domain/DangerState.java
+++ src/main/java/com/example/demo/emotion/domain/DangerState.java
@@ -1,5 +1,32 @@
 package com.example.demo.emotion.domain;
 
+import com.example.demo.chat.infrastructure.jpa.LongTermMemory;
+
 public enum DangerState {
-    CHRONIC, BURST, HIGH_DANGER, STABLE;
+    HIGH_DANGER {
+        @Override
+        public void adjust(LongTermMemory memory) {
+            memory.appendHighDangerMessage();
+        }
+    },
+    BURST {
+        @Override
+        public void adjust(LongTermMemory memory) {
+            memory.appendBurstDangerMessage();
+        }
+    },
+    CHRONIC {
+        @Override
+        public void adjust(LongTermMemory memory) {
+            memory.appendChronicDangerMessage();
+        }
+    },
+    STABLE {
+        @Override
+        public void adjust(LongTermMemory memory) {
+            memory.clearDangerMessage();
+        }
+    };
+
+    public abstract void adjust(LongTermMemory memory);
 }
