67c94b4942e883e8835702f2bd94844a5f03f11e
thstkddnr20 <thstkddnr20@gmail.com>
2025-10-20 16:04:18 +0900

    refactor: adminService의 runtimeException 제거

--- src/main/java/com/example/demo/exception/business/errorcode/AdminErrorCode.java
+++ src/main/java/com/example/demo/exception/business/errorcode/AdminErrorCode.java
@@ -7,7 +7,14 @@ import org.springframework.http.HttpStatus;
 public enum AdminErrorCode implements ErrorCode {
 
     ADMIN_ACCOUNT_NOT_EXIST(HttpStatus.UNAUTHORIZED, "ADMIN_ACCOUNT_NOT_EXIST", "관리자 계정이 존재하지 않습니다."),
-    ADMIN_PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED, "ADMIN_PASSWORD_NOT_MATCH", "비밀번호가 틀렸습니다.");
+    ADMIN_PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED, "ADMIN_PASSWORD_NOT_MATCH", "비밀번호가 틀렸습니다."),
+
+    INVALID_FILTER_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_FILTER_REQUEST", "WAITING 상태의 프로모션만 필터링할 수 있습니다."),
+    INVALID_PROMOTE_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_PROMOTE_REQUEST", "FILTERED 상태의 미션만 승격할 수 있습니다."),
+    PROMOTION_NOT_FOUND(HttpStatus.BAD_REQUEST, "PROMOTION_NOT_FOUND", "미션 프로모션을 찾을 수 없습니다."),
+
+    PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST, "PRODUCT_NOT_FOUND", "상품을 찾을 수 없습니다.");
+
 
     private final HttpStatus status;
     private final String code;
