ce2e17d969be6f3cd18e813a215ce40edab17393
surim <su020900@gmail.com>
2025-10-30 23:28:34 +0900

    refact:

--- src/main/java/com/example/demo/mission/controller/dto/MissionResponse.java
+++ src/main/java/com/example/demo/mission/controller/dto/MissionResponse.java
@@ -3,10 +3,8 @@ package com.example.demo.mission.controller.dto;
 import com.example.demo.mission.Mission;
 import com.example.demo.mission.MissionCategoryEnum;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@ToString
 public class MissionResponse {
 
     private final Long id;
